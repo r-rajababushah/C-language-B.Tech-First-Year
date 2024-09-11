b1d31492302fdb7fdaf0dbe9fea454b62569ad20
Rajababu Shah <rajababushah.in@gmail.com>
2024-09-11 14:15:25 +0530

    FLLLLLLL

--- test.c
+++ test.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    char a = 'a';
-    printf("%d", sizeof(a));
+    printf("Input the character to test wheather its capital case or lower case : ");
+    char inputChar, asciiChar;
+    scanf("%c", &inputChar);
+
+    int flag;
+
+    for(int i=65; i<90; i++){
+        asciiChar = (char) i;
+        if(asciiChar == inputChar){
+            flag = 1;
+            break;
+        }else{
+            flag = 0;
+        }
+    }
+
+    if(flag)
+        printf("The Character is UPPERCASE \n");
+    else 
+        printf("The character is UPPERCASE \n");
     return 0;
 }
 
